c22f8bdc73a117ddcb722ca201cc0e1be2fa074e
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2024-01-19 13:52:12 -0700

    cancels the file download

--- servicework.js
+++ servicework.js
@@ -1,4 +1,7 @@
 chrome.downloads.onDeterminingFilename.addListener(function (file) {
     const allowed_files = ['video/mp4', 'video/webm', 'video/x-m4v', 'video/quicktime']
-    if (allowed_files.includes(file.mime)) {chrome.tabs.create({url: `/displayer.html?url=${file.url}`})};
+    if (allowed_files.includes(file.mime)) {
+        chrome.downloads.cancel(file.id)
+        chrome.tabs.create({url: `/displayer.html?url=${file.url}`})
+    };
 });
